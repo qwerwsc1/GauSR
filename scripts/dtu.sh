fcde398c3f0e1fc1ea84f205f6ab35ae37af7ad5
qwerwsc1 <sichennng@gmail.com>
2026-06-12 14:12:18 +0900

    w/o multi-view consistency

--- scripts/dtu.sh
+++ scripts/dtu.sh
@@ -2,7 +2,7 @@ dataset_folder=/home/wangsc/Documents/datasets/dtu_dataset/dtu
 output_folder=/media/data/SurR/outputs/gausr/dtu_wo-mvc
 for scene in 24 37 40 55 63 65 69 83 97 105 106 110 114 118 122
 do
-    python train.py -s ${dataset_folder}/scan${scene} -m output_folder/scan${scene} -r 2 --use_decoupled_appearance 3
-    python mesh_extract.py -m output_folder/scan${scene}
-    python evaluate_dtu_mesh.py -m output_folder/scan${scene}
+    python train.py -s ${dataset_folder}/scan${scene} -m {output_folder}/scan${scene} -r 2 --use_decoupled_appearance 3
+    python mesh_extract.py -m {output_folder}/scan${scene}
+    python evaluate_dtu_mesh.py -m {output_folder}/scan${scene}
 done
