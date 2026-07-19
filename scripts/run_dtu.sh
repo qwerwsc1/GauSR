4a187aea22cd50501ddf3e4cfa08549cf46eef6a
qwerwsc1 <sichennng@gmail.com>
2026-07-19 18:40:40 +0900

    add reconstruction codes

--- scripts/run_dtu.sh
+++ scripts/run_dtu.sh
@@ -1,7 +1,7 @@
 dataset_folder=/home/wangsc/Documents/datasets/dtu_dataset/dtu/
 for scene in 24 37 40 55 63 65 69 83 97 105 106 110 114 118 122
 do
-    python train.py -s ${dataset_folder}/scan${scene} -m /media/data/SurR/outputs/gausr/dtu/scan${scene} -r 2
+    # python train.py -s ${dataset_folder}/scan${scene} -m /media/data/SurR/outputs/gausr/dtu/scan${scene} -r 2
     python mesh_extract.py -m /media/data/SurR/outputs/gausr/dtu/scan${scene}
-    python evaluate_dtu_mesh.py -m /media/data/SurR/outputs/gausr/dtu/scan${scene}
+    python scripts/eval_dtu/evaluate_dtu_mesh.py -m /media/data/SurR/outputs/gausr/dtu/scan${scene}
 done
