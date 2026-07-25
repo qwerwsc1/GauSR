553f530521e7a4077aae514bcbe44c17eb506b6c
qwerwsc1 <sichennng@gmail.com>
2026-07-25 22:31:55 +0900

    test in translab dataset

---
+++ scripts/run_translab.sh
@@ -0,0 +1,8 @@
+dataset_folder=/home/wangsc/Documents/datasets/translab/
+output_folder=/media/data/SurR/outputs/gausr/trans-mvg-aspt
+for scene in 05 06 07 08
+do
+    python train.py -s ${dataset_folder}/scene_${scene} -m ${output_folder}/scene_${scene} -r 2
+    python mesh_extract.py -m ${output_folder}/scene_${scene}
+    python scripts/eval_dtu/evaluate_dtu_mesh.py -m ${output_folder}/scene_${scene}
+done
