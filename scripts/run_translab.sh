994694247ad496b9b84dcf8f5d2c6ce8f1f79832
qwerwsc1 <sichennng@gmail.com>
2026-07-25 23:36:32 +0900

    test in translab dataset

--- scripts/run_translab.sh
+++ scripts/run_translab.sh
@@ -5,5 +5,5 @@ for scene in 05 06 07 08
 do
     # python train.py -s ${dataset_folder}/scene_${scene} -m ${output_folder}/scene_${scene} -r 2
     # python mesh_extract.py -m ${output_folder}/scene_${scene}
-    python scripts/eval_translab/eval.py -m ${output_folder}/scene_${scene}
+    python scripts/eval_translab/eval.py --data ${output_folder}/scene_${scene}/recon_post.ply --scan scene_${scene} --vis_out_dir ${output_folder}/scene_${scene} --dataset_dir ${dataset_folder}/scene_${scene} --mode mesh --downsample_density 0.002
 done
