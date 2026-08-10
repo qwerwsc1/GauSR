c9c7782e859d081d103c474499a86af07f88d5e8
qwerwsc1 <sichennng@gmail.com>
2026-08-11 04:56:49 +0900

    add tnt dataset

--- train.py
+++ train.py
@@ -258,7 +258,7 @@ if __name__ == "__main__":
     op = OptimizationParams(parser)
     pp = PipelineParams(parser)
     parser.add_argument('--ip', type=str, default="127.0.0.1")
-    parser.add_argument('--port', type=int, default=6009)
+    parser.add_argument('--port', type=int, default=6008)
     parser.add_argument('--debug_from', type=int, default=-1)
     parser.add_argument('--detect_anomaly', action='store_true', default=False)
     parser.add_argument("--test_iterations", nargs="+", type=int, default=[7_000, 30_000])
